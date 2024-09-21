0d59e2a855cc42fe46e337dfa2fe1b637a06960e
sazzad369 <mdsazzad0369@gmail.com>
2024-09-21 10:04:51 +0600

    kamrul gay

---
+++ app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.viewmodeldemo
+
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.viewmodeldemo.databinding.ActivityMainBinding
+import com.example.viewmodeldemo.viewmodel.CounterViewModel
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val counterViewModel : CounterViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.textView.text = counterViewModel.counter.toString()
+        binding.countbtn.setOnClickListener {
+            counterViewModel.increment()
+            binding.textView.text = counterViewModel.counter.toString()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/viewmodeldemo/viewmodel/CounterViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.viewmodeldemo.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel() {
+    var counter: Int = 0
+    fun increment() {
+        counter++
+    }
+}
